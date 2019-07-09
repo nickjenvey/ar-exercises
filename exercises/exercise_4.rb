3c681be592b434a1d24f948d1b509d1c7a9d03fb
Nick <nbjenvey@gmail.com>
2019-07-09 21:22:46 +0000

    Exercise 4 complete

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,23 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+surrey = Store.create :name => 'Surrey', :annual_revenue => 224000, :mens_apparel => false, :womens_apparel => true
+whistler = Store.create :name => 'Whistler', :annual_revenue => 1900000, :mens_apparel => true, :womens_apparel => false
+yaletown = Store.create :name => 'Yaletown', :annual_revenue => 430000, :mens_apparel => true, :womens_apparel => true
+
+@mens_stores = Store.where(mens_apparel: true)
+
+@mens_stores.each do |s|
+  puts "Store Name: #{s.name}, Annual Revenue: #{s.annual_revenue}"
+end
+
+@womens_stores = Store.where(womens_apparel: true)
+
+less_then_1mil = @womens_stores.select do |s|
+  s.annual_revenue < 1000000 
+end
+
+less_then_1mil.each do |s|
+  puts "Store Name: #{s.name}, Annual Revenue: #{s.annual_revenue}"
+end
